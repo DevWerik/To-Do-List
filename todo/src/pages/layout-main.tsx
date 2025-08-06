5fcb0c03f7525c9a1f4ecedbbd4578fb5e9723cb
DevWerik <werikanjos21@gmail.com>
2025-08-06 20:19:15 -0300

    fix:Structuring the layout body

--- todo/src/pages/layout-main.tsx
+++ todo/src/pages/layout-main.tsx
@@ -1,28 +1,16 @@
-import Container from "../components/container";
-import { NavLink, Outlet } from "react-router";
-import Text from "../components/text";
+import { Outlet } from "react-router";
+import Header from "../core-components/header";
+import MainContent from "../core-components/main-content";
+import Footer from "../core-components/footer";
 
 export default function LayoutMain(){
     return (
       <>
-        <Container as="header" className="mt-3 md:mt20">Ola mundo - Header</Container>
-        <main className="mt-4 md-mt8">
-            <Outlet />
-        </main>
-        <footer className="my-5 md:my-10">
-          <nav className="flex items-center justify-center gap-4">
-            <NavLink to="/">
-              <Text variant="body-sm-bold" className="text-gray-300">
-                tarefas
-              </Text>
-            </NavLink>
-            <NavLink to="/componentes">
-              <Text variant="body-sm-bold" className="text-gray-300">
-                tarefas
-              </Text>
-            </NavLink>
-          </nav>
-        </footer>
+        <Header />
+        <MainContent className="mt-4 md:mt-8">
+          <Outlet />
+        </MainContent>
+        <Footer />
       </>
     );
 
