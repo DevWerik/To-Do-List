6abb79566053f5a0200b083b2bbbd1da8d1360b3
DevWerik <werikanjos21@gmail.com>
2025-08-06 15:51:02 -0300

    fix: Creating Skeleton Component

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -12,48 +12,60 @@ import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input-text";
 import InputCheckbox from "./components/input-checkbox";
 import Card from "./components/card";
+import Container from "./components/container";
+import Skeleton from "./components/skeleton";
 
 export default function App() {
   return (
-    <div className="grid gap-3">
-      <div className="flex flex-col gap-2">
-        <Text variant="body-sm-bold" className="text-pink-base">
-          Ola mundo
-        </Text>
-        <Text className="text-pink-base">Ola mundo </Text>
-        <Text variant="body-md-bold">Ola mundo </Text>
-      </div>
-      <div className="felx gap-1">
-        <Icon svg={Vector} className="fill-green-base" />
-        <Icon svg={Check} />
-        <Icon svg={Pencil} />
-        <Icon svg={Plus} />
-        <Icon svg={spinner} animate />
-        <Icon svg={X} />
-      </div>
+    <Container>
+      <div className="grid gap-10">
+        <div className="flex flex-col gap-2">
+          <Text variant="body-sm-bold" className="text-pink-base">
+            Ola mundo
+          </Text>
+          <Text className="text-pink-base">Ola mundo </Text>
+          <Text variant="body-md-bold">Ola mundo </Text>
+        </div>
+        <div className="felx gap-1">
+          <Icon svg={Vector} className="fill-green-base" />
+          <Icon svg={Check} />
+          <Icon svg={Pencil} />
+          <Icon svg={Plus} />
+          <Icon svg={spinner} animate />
+          <Icon svg={X} />
+        </div>
 
-      <div>
-        <Badge variant="secondary">5</Badge>
-        <Badge variant="primary">2 de 5</Badge>
-      </div>
+        <div className="flex gap-1">
+          <Badge variant="secondary">5</Badge>
+          <Badge variant="primary">2 de 5</Badge>
+          <Badge loadinng>5</Badge>
+        </div>
 
-      <div>
-        <Button icon={Plus}>Nova tarefa</Button>
+        <div>
+          <Button icon={Plus}>Nova tarefa</Button>
+        </div>
+        <div className="flex gap-1">
+          <ButtonIcon icon={Vector} />
+          <ButtonIcon icon={Vector} variant={"secondary"} />
+          <ButtonIcon icon={Vector} variant={"tertiary"} />
+          <ButtonIcon icon={Vector} loading />
+        </div>
+        <div>
+          <InputText />
+        </div>
+        <div>
+          <InputCheckbox />
+          <InputCheckbox loading/>
+          
+        </div>
+        <div>
+          <Card size="md">Olá mundo</Card>
+        </div>
+        <div className="space-y-2"></div>
+        <Skeleton className="h-6" />
+        <Skeleton className="h-6" />
+        <Skeleton className="w-96" />
       </div>
-      <div className="flex gap-1">
-        <ButtonIcon icon={Vector} />
-        <ButtonIcon icon={Vector} variant={"secondary"} />
-        <ButtonIcon icon={Vector} variant={"tertiary"} />
-      </div>
-      <div>
-        <InputText/>
-      </div>
-      <div>
-        <InputCheckbox />
-      </div>
-      <div>
-       <Card size="md">Olá mundo</Card>
-      </div>
-    </div>
+    </Container>
   );
 }
--- todo/src/components/badge.tsx
+++ todo/src/components/badge.tsx
@@ -1,42 +1,80 @@
 import React from "react";
-import Text from "./text";0
-import { cva, type VariantProps } from "class-variance-authority";
+import Text from "./text";
+0;
+import { cva, cx, type VariantProps } from "class-variance-authority";
+import Skeleton from "./skeleton";
 
 export const badgeVariants = cva(
   "inline-flex intems-center justify-center rounded-full",
   {
     variants: {
       variant: {
+        none: "",
         primary: "bg-green-light",
         secondary: "bg-pink-light",
-     },
-     size:{
-        sm:"py-0.5 px-2"
-     }
+      },
+      size: {
+        sm: "py-0.5 px-2",
+      },
     },
     defaultVariants: {
       variant: "primary",
-      size:"sm"
-    }
-  });
-
-  export const BadgetextVariants = cva("", {
-    variants: {
-      variant: {
-        primary: "text-green-dark",
-        secondary: "text-pink-dark",
-      }, 
+      size: "sm",
     },
-    defaultVariants: {
-      variant: "primary",
-    }
   }
-  )
+);
+
+export const BadgetextVariants = cva("", {
+  variants: {
+    variant: {
+      primary: "text-green-dark",
+      secondary: "text-pink-dark",
+      none: "",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+});
+
+export const badgeSkeletonVariants = cva("", {
+  variants: {
+    size: {
+      sm: "w-6 h-6",
+    },
+  },
+  defaultVariants: {
+    size: "sm",
+  },
+});
 interface BadgeProps
   extends React.ComponentProps<"div">,
-    VariantProps<typeof badgeVariants> {}
+    VariantProps<typeof badgeVariants> {
+  loadinng?: boolean;
+  children?: React.ReactNode;
+}
 
-export default function Badge({ variant, size, className, children, ...props }: BadgeProps) {
+export default function Badge({
+  variant,
+  size,
+  className,
+  children,
+  loadinng,
+  ...props
+}: BadgeProps) {
+  if (loadinng) {
+    return (
+      <Skeleton
+        rounded="full"
+        className={cx(
+          badgeVariants({ variant: "none" }),
+          badgeSkeletonVariants({ size }),
+          className
+        )}
+      />
+    );
+  }
+
   return (
     <div className={badgeVariants({ variant, size, className })} {...props}>
       <Text variant="body-sm-bold" className={BadgetextVariants({ variant })}>
