333365438c82ec8801e71da3c94fe49a82f0f670
DevWerik <werikanjos21@gmail.com>
2025-08-07 19:53:06 -0300

    fix: collecting values from a form in React manually using states and we will implement an onChange event to update this state

--- todo/src/core-components/task-item.tsx
+++ todo/src/core-components/task-item.tsx
@@ -10,13 +10,18 @@ import Check from "../assets/icons/Check.svg?react";
 import InputText from "../components/input-text";
 import { TaskState, type Task } from "../models/task";
 import { cx } from "class-variance-authority";
+import { Form } from "react-router";
 
 interface TaskItemProps {
   task: Task;
 }
 
 export default function TaskItem({ task }: TaskItemProps) {
-  const [isEditing, setIsEditing] = React.useState(task?.state === TaskState.Creating);
+  const [isEditing, setIsEditing] = React.useState(
+    task?.state === TaskState.Creating
+  );
+
+  const [taskTitle, setTaskTitle] = React.useState("");
 
   function handleEditTask() {
     setIsEditing(true);
@@ -26,40 +31,64 @@ export default function TaskItem({ task }: TaskItemProps) {
     setIsEditing(false);
   }
 
+  function handleChangeTaskTitle(e: React.ChangeEvent<HTMLInputElement>) {
+    setTaskTitle(e.target.value || "");
+  }
+
+  function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log({id: task.id, title: taskTitle});
+    //Chamada para a função de atualizar o estado do task
+    setIsEditing(false);
+  } 
+
   return (
-    <Card size="md" className="flex items-center gap-4">
-      {!isEditing ? (
-        <>
-          <InputChekbox
-            value={task?.concluded?.toString()}
-            checked={task?.concluded}
-          />
-          <Text
-            className={cx("flex-1", {
-              "line-through": task?.concluded,
-            })}
-          >
-            {" "}
-            {task?.title}{" "}
-          </Text>
-          <div className="flex gap-1">
-            <ButtonIcon icon={Vector} variant="tertiary" />
-            <ButtonIcon
-              icon={Pencil}
-              variant="tertiary"
-              onClick={handleEditTask}
+   
+      <Card size="md" >
+        {!isEditing ? (
+          <div className="flex items-center gap-4">
+            <InputChekbox
+              value={task?.concluded?.toString()}
+              checked={task?.concluded}
             />
+            <Text
+              className={cx("flex-1", {
+                "line-through": task?.concluded,
+              })}
+            >
+              {" "}
+              {task?.title}{" "}
+            </Text>
+            <div className="flex gap-1">
+              <ButtonIcon type="button" icon={Vector} variant="tertiary" />
+              <ButtonIcon
+              type="button"
+                icon={Pencil}
+                variant="tertiary"
+                onClick={handleEditTask}
+              />
+            </div>
           </div>
-        </>
-      ) : (
-        <>
-          <InputText className="flex-1" />
-          <div className="flex gap-1">
-            <ButtonIcon icon={X} variant="secondary" onClick={handleExitTask} />
-            <ButtonIcon icon={Check} variant="primary" />
-          </div>
-        </>
-      )}
-    </Card>
+        ) : (
+          <form onSubmit={handleSaveTask} className="flex items-center gap-4">
+            <InputText
+              className="flex-1"
+              onChange={handleChangeTaskTitle}
+              required
+              autoFocus
+            />
+            <div className="flex gap-1">
+              <ButtonIcon
+                typeof="button"
+                icon={X}
+                variant="secondary"
+                onClick={handleExitTask}
+              />
+              <ButtonIcon type="submit" icon={Check} variant="primary" />
+            </div>
+          </form>
+        )}
+      </Card>
+    
   );
 }
