f10ffd194e6c94516682487ec06847b0a13c5a1e
DevWerik <werikanjos21@gmail.com>
2025-08-06 22:00:12 -0300

    fix: develop a function to prepare the creation of tasks. When we click on "new task"

---
+++ todo/src/hooks/use.task.ts
@@ -0,0 +1,22 @@
+import useLocalStorage from "use-local-storage";
+import { TASKS_KEY } from "../models/task";
+import type { Task } from "../models/task";
+import { TaskState } from "../models/task";
+
+export default function useTask() {
+    const [task, setTask] = useLocalStorage<Task[]>(TASKS_KEY, []);
+
+    function prepareTask() {
+        setTask([
+          ...task,
+          {
+            id: Math.random().toString(36).substring(2.9),
+            title: "",
+            state: TaskState.Creating,
+          },
+        ]);
+    }
+    return {
+        prepareTask,
+    }
+}
